facd1a05a7f81579038fbb27ab7034574f8a261e
Timm Friebe <thekid@thekid.de>
2013-04-20 12:53:59 +0200

    - Register vendor and modules on recieving payload

--- dist/net/xp_framework/build/api/WebHook.class.php
+++ dist/net/xp_framework/build/api/WebHook.class.php
@@ -1,5 +1,7 @@
-<?php uses('webservices.rest.srv.Response', 'webservices.rest.RestFormat', 'util.log.LogCategory', 'util.Properties', 'io.streams.MemoryInputStream', 'org.codehaus.stomp.StompConnection', 'net.xp_framework.build.api.GitHubPayload', 'net.xp_framework.build.api.GitHubRepository', 'net.xp_framework.build.api.GitHubUserReference');
+<?php uses('webservices.rest.srv.Response', 'webservices.rest.RestFormat', 'util.log.LogCategory', 'util.Properties', 'io.IOException', 'io.streams.MemoryInputStream', 'io.collections.FileCollection', 'org.codehaus.stomp.StompConnection', 'net.xp_framework.build.api.GitHubPayload', 'net.xp_framework.build.api.GitHubRepository', 'net.xp_framework.build.api.GitHubUserReference');
 
+;
+;
 ;
 ;
 ;
@@ -17,7 +19,10 @@
  class WebHook extends Object{
 private $cat;
 private $queue;
+private $storage;
 private $destination;
+
+private static $json;
 
 
 
@@ -40,6 +45,14 @@ $this->destination=$prop->readString('destinations','trigger');}
 
 
 
+
+public function useStorage(Properties $prop){
+$this->storage=new FileCollection($prop->readString('storage','folder','releases'));}
+
+
+
+
+
 public function __destruct(){
 $this->queue->disconnect();}
 
@@ -54,7 +67,36 @@ public function githubTrigger($in){
 try {
 $payload=cast(RestFormat::$JSON->read(new MemoryInputStream($in),XPClass::forName('net.xp_framework.build.api.GitHubPayload')), 'net.xp_framework.build.api.GitHubPayload');} catch(FormatException $e) {
 
+$this->cat&&$this->cat->warn('Malformed payload',$e);
 return Response::error(400)->withPayload('Malformed payload: '.$e->compoundMessage());};
+
+
+
+try {
+if (!$vendor=$this->storage->findCollection($payload->repository->owner->name)) {
+$this->cat&&$this->cat->info('New vendor',$payload->repository->owner);
+$vendor=$this->storage->newCollection($payload->repository->owner->name);};
+
+
+if (!$module=$vendor->findCollection($payload->repository->name)) {
+$this->cat&&$this->cat->info('New module',$payload->repository);
+$module=$vendor->newCollection($payload->repository->name);};
+
+
+if (!$info=$module->findElement('module.json')) {
+$info=$module->newElement('module.json');};
+
+
+
+
+
+
+
+
+
+$out=$info->getOutputStream();$··e= NULL; try {$out->write(WebHook::$json->serialize(array('vendor' => $payload->repository->owner->name,'module' => $payload->repository->name,'info' => $payload->repository->description,'link' => $payload->repository->url,)));} catch (Exception $··e) {}try { $out->close(); } catch (Exception $··i) {}if ($··e) throw $··e;;} catch(IOException $e) {
+
+$this->cat&&$this->cat->warn('Storage error, continuing anyways',$e);};
 
 
 
@@ -67,18 +109,16 @@ sscanf($tag,'r%[0-9A-Za-z.~]',$version);
 
 
 
+$message=WebHook::$json->serialize(array('owner' => $payload->repository->owner->name,'repo' => $payload->repository->name,'tag' => $tag,'version' => $version,'user' => $payload->pusher->name,));
+$this->cat&&$this->cat->info($this->destination,$message);
+$this->queue->send($this->destination,$message,
+array('content-type' => WebHook::$json->contentType(),));}else {
 
-
-
-
-
-
-$serializer=RestFormat::$JSON->serializer();$message=$serializer->serialize(array('owner' => $payload->repository->owner->name,'repo' => $payload->repository->name,'tag' => $tag,'version' => $version,'user' => $payload->pusher->name,));$this->cat&&$this->cat->info($this->destination,$message);$this->queue->send($this->destination,$message,array('content-type' => $serializer->contentType(),));;}else {
 
 $this->cat&&$this->cat->debug('Ignore',$payload);};
 
 
-return Response::created();}}xp::$registry['class.WebHook']= 'net.xp_framework.build.api.WebHook';xp::$registry['details.net.xp_framework.build.api.WebHook']= array (
+return Response::created();}static function __static() {WebHook::$json=RestFormat::$JSON->serializer();}}xp::$registry['class.WebHook']= 'net.xp_framework.build.api.WebHook';xp::$registry['details.net.xp_framework.build.api.WebHook']= array (
   0 => 
   array (
     'cat' => 
@@ -95,11 +135,25 @@ return Response::created();}}xp::$registry['class.WebHook']= 'net.xp_framework.b
         'type' => 'org.codehaus.stomp.StompConnection',
       ),
     ),
+    'storage' => 
+    array (
+      5 => 
+      array (
+        'type' => 'io.collections.FileCollection',
+      ),
+    ),
     'destination' => 
     array (
       5 => 
       array (
         'type' => 'string',
+      ),
+    ),
+    'json' => 
+    array (
+      5 => 
+      array (
+        'type' => 'var',
       ),
     ),
   ),
@@ -143,6 +197,28 @@ return Response::created();}}xp::$registry['class.WebHook']= 'net.xp_framework.b
         'inject' => 
         array (
           'name' => 'mq',
+        ),
+      ),
+      6 => 
+      array (
+      ),
+    ),
+    'useStorage' => 
+    array (
+      1 => 
+      array (
+        0 => 'util.Properties',
+      ),
+      2 => 'void',
+      3 => 
+      array (
+      ),
+      4 => 'Use configuration to inject release storage',
+      5 => 
+      array (
+        'inject' => 
+        array (
+          'name' => 'xarrelease',
         ),
       ),
       6 => 
