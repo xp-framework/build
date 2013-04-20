9239d84051bf2c92fb529fa49576319ad10f7aca
Timm Friebe <thekid@thekid.de>
2013-04-20 11:11:35 +0200

    - Initial release

---
+++ dist/net/xp_framework/build/api/Search.class.php
@@ -0,0 +1,82 @@
+<?php uses('io.collections.FileCollection', 'io.collections.iterate.FilteredIOCollectionIterator', 'io.collections.iterate.CollectionFilter', 'io.collections.iterate.AllOfFilter', 'io.collections.iterate.NameMatchesFilter', 'text.regex.Pattern', 'net.xp_framework.build.api.AbstractBuildInformation');
+
+;
+;
+;
+;
+;
+;
+
+;
+
+
+ class Search extends net·xp_framework·build·api·AbstractBuildInformation{
+
+
+
+
+
+
+
+
+public function forModules($query){
+
+
+
+$find=new AllOfFilter(array(new CollectionFilter(),new NameMatchesFilter(new Pattern($query,Pattern::CASE_INSENSITIVE)),));
+
+$found=array();
+foreach (new FilteredIOCollectionIterator($this->storage,new CollectionFilter()) as $vendor) {
+foreach (new FilteredIOCollectionIterator($vendor,$find) as $module) {
+
+
+
+$found[]=array('vendor' => basename($vendor->getURI()),'module' => basename($module->getURI()),);};};
+
+
+return $found;}}xp::$registry['class.Search']= 'net.xp_framework.build.api.Search';xp::$registry['details.net.xp_framework.build.api.Search']= array (
+  0 => 
+  array (
+  ),
+  1 => 
+  array (
+    'forModules' => 
+    array (
+      1 => 
+      array (
+        0 => 'string',
+      ),
+      2 => 'var[]',
+      3 => 
+      array (
+      ),
+      4 => 'Searches for modules',
+      5 => 
+      array (
+        'webmethod' => 
+        array (
+          'verb' => 'GET',
+        ),
+      ),
+      6 => 
+      array (
+        '$query' => 
+        array (
+          'param' => 'q',
+        ),
+      ),
+    ),
+  ),
+  'class' => 
+  array (
+    5 => 
+    array (
+      'webservice' => 
+      array (
+        'path' => '/search',
+      ),
+    ),
+    4 => NULL,
+  ),
+);
+?>
