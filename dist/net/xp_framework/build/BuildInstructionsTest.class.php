7955d7ad04fdecc16dd6c68ec66c28f10db6fd5b
Timm Friebe <thekid@thekid.de>
2013-10-06 14:37:02 +0200

    - Refactor: Add assertAccessors() helper

--- dist/net/xp_framework/build/BuildInstructionsTest.class.php
+++ dist/net/xp_framework/build/BuildInstructionsTest.class.php
@@ -1,35 +1,60 @@
-<?php uses('unittest.TestCase', 'net.xp_framework.build.BuildInstructions');
+<?php uses('unittest.TestCase', 'lang.Generic', 'net.xp_framework.build.BuildInstructions');
 
  class BuildInstructionsTest extends TestCase{
+
+
+
+
+
+
+
+
+protected function assertAccessors(Generic $instance,$member,$value){
+
+
+
+$class=$instance->getClass();$class->getMethod('set'.$member)->invoke($instance,array($value,));$this->assertEquals($value,$class->getMethod('get'.$member)->invoke($instance,array()));;}
+
 
 public function can_create(){
 new BuildInstructions();}
 
 
 public function base_accessors(){
-$base='.';
-$b=new BuildInstructions();
-$b->setBase($base);
-$this->assertEquals($base,$b->getBase());}
+$this->assertAccessors(new BuildInstructions(),'base','.');}
 
 
 public function naming_accessors(){
-$naming=array('name' => 'test',);
-$b=new BuildInstructions();
-$b->setNaming($naming);
-$this->assertEquals($naming,$b->getNaming());}
+$this->assertAccessors(new BuildInstructions(),'naming',array('name' => 'test',));}
 
 
 public function finalize_accessors(){
-$finalize='test';
-$b=new BuildInstructions();
-$b->setFinalize($finalize);
-$this->assertEquals($finalize,$b->getFinalize());}}xp::$cn['BuildInstructionsTest']= 'net.xp_framework.build.BuildInstructionsTest';xp::$meta['net.xp_framework.build.BuildInstructionsTest']= array (
+$this->assertAccessors(new BuildInstructions(),'finalize','test');}}xp::$cn['BuildInstructionsTest']= 'net.xp_framework.build.BuildInstructionsTest';xp::$meta['net.xp_framework.build.BuildInstructionsTest']= array (
   0 => 
   array (
   ),
   1 => 
   array (
+    'assertAccessors' => 
+    array (
+      1 => 
+      array (
+        0 => 'lang.Generic',
+        1 => 'string',
+        2 => 'var',
+      ),
+      2 => 'void',
+      3 => 
+      array (
+      ),
+      4 => 'Test accessors for a given member (getX and setX).',
+      5 => 
+      array (
+      ),
+      6 => 
+      array (
+      ),
+    ),
     'can_create' => 
     array (
       1 => 
